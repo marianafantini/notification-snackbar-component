271ccaf5dd8d603ecffe6cdfc3a17f6f929b76e8
Mariana Fantini <mariana.fantini@gmail.com>
2020-04-20 01:33:23 -0300

    Snackbar is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, Injector } from "@angular/core";
 import { createCustomElement } from "@angular/elements";
 import { AppComponent } from "./app.component";
-import { ToastComponent } from "./toast/toast.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { SnackbarTemplateComponent } from './snackbar-template/snackbar-template.component';
 import { SnackbarComponent } from './snackbar/snackbar.component';
 
 @NgModule({
-  declarations: [AppComponent, ToastComponent, SnackbarTemplateComponent, SnackbarComponent],
+  declarations: [AppComponent, SnackbarTemplateComponent, SnackbarComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MatSnackBarModule],
   providers: [],
-  bootstrap: [AppComponent],
-  // entryComponents: [ToastComponent]
+  // bootstrap: [AppComponent],
+  entryComponents: [SnackbarComponent]
 })
 export class AppModule {
-  // constructor(injector: Injector) {
-  //   // define custom component using the HelloWorldComponent
-  //   const custom = createCustomElement(ToastComponent, { injector: injector });
-  //   // define custom component's tag
-  //   customElements.define('app-toast', custom);
-  // }
-  // ngDoBootstrap() { }
+  constructor(injector: Injector) {
+    // define custom component using the SnackbarComponent
+    const custom = createCustomElement(SnackbarComponent, { injector: injector });
+    // define custom component's tag
+    customElements.define('app-snackbar', custom);
+  }
+  ngDoBootstrap() { }
 }
--- src/app/snackbar/snackbar.component.ts
+++ src/app/snackbar/snackbar.component.ts
@@ -1,23 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar'; 
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+  MatSnackBar,
+  MatSnackBarConfig,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-snackbar',
-  templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.scss']
+  selector: "app-snackbar",
+  templateUrl: "./snackbar.component.html",
+  styleUrls: ["./snackbar.component.scss"],
 })
 export class SnackbarComponent implements OnInit {
+  @Input() snackbarMessage: string = "Message here";
+  @Input() snackbarAction: string = "Action";
+  @Input() snackbarHorizontalPosition: MatSnackBarHorizontalPosition = "right";
+  @Input() snackbarVerticalPosition: MatSnackBarVerticalPosition = "bottom";
+  @Output() actionClicked: EventEmitter<any> = new EventEmitter();
 
-  constructor(private snackbar: MatSnackBar) { }
+  snackbarConfig: MatSnackBarConfig = {
+    horizontalPosition: this.snackbarHorizontalPosition,
+    verticalPosition: this.snackbarVerticalPosition,
+  };
+
+  constructor(private snackbar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.openSnackbar();
   }
 
   openSnackbar(): void {
-    let snackbarRef = this.snackbar.open('Message archived', 'teste');
-    console.log("done")
+    let snackbarRef = this.snackbar.open(
+      this.snackbarMessage,
+      this.snackbarAction,
+      this.snackbarConfig
+    );
 
+    snackbarRef.onAction().subscribe(() => {
+      console.log("action happened");
+      this.actionClicked.emit(true);
+    });
+    
   }
 
 }
